5cf6e8dc6db810acedbacdb3f50680fc1ce8e3ef
Rafael Marangoni Waterkemper <rafaelwaterkemper@gmail.com>
2022-04-27 16:24:00 -0300

    Testing removing require-approval never

--- src/lambdas/simple-api/index.js
+++ src/lambdas/simple-api/index.js
@@ -2,7 +2,7 @@ module.exports.handler = async function handler(event) {
   try {
     return {
       statusCode: 200,
-      body: event.requestContext.domainName,
+      body: "Hello world from AWS"
     };
   } catch (err) {
     return {
