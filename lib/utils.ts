4c05cc9801b8abb92623fc74fc7baf18c7f2262a
Rafael Marangoni Waterkemper <rafaelwaterkemper@gmail.com>
2022-05-03 23:18:40 -0300

    Removed the qualifier

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,7 +3,7 @@ export function capitalize(text: string) {
 }
 
 export function getNamespace(): string {
-  return <string>process.env.NAMESPACE || "development";
+  return "development";
 }
 
 export function getResourceName(baseName: string) {
